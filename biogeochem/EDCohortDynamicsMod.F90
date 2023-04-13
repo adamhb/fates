5cdefddae9e852712456b05e77daef1192d39801
Adam Hanbury-Brown <ahanburybrown@gmail.com>
2023-04-13 11:55:50 -0600

    changed pcc attribute to intentin in the insert_cohort subroutine

--- biogeochem/EDCohortDynamicsMod.F90
+++ biogeochem/EDCohortDynamicsMod.F90
@@ -1758,8 +1758,8 @@ contains
     ! !USES:
     !
     ! !ARGUMENTS
-    type(ed_cohort_type) , intent(inout), pointer :: pcc
-    type(ed_cohort_type) , intent(inout), pointer :: ptall
+    type(ed_cohort_type) , intent(in), pointer :: pcc
+    type(ed_cohort_type) , intent(in), pointer :: ptall
     type(ed_cohort_type) , intent(inout), pointer :: pshort
     integer              , intent(in)                     :: tnull
     integer              , intent(in)                     :: snull
