e9b5449912d309d48df39445863665d6527d0e7e
Adam Hanbury-Brown <ahanburybrown@gmail.com>
2023-10-04 17:26:48 -0600

    wrote new resprout debug flags

--- biogeochem/EDPatchDynamicsMod.F90
+++ biogeochem/EDPatchDynamicsMod.F90
@@ -1019,6 +1019,8 @@ contains
                                nrc%n = currentCohort%n * patch_site_areadis/currentPatch%area * &
                                        currentCohort%frac_resprout
                                
+                               write(fates_log(),*) "nrc_n", nrc%n
+
 			       ! Transfer over the mortality rates for diagnostics
                                ! Caution: absolute mortality numbers will change when cohort number
 			       ! densities change
